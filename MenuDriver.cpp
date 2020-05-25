ad1b6d6b32bc3cd95a5e0ba9737a6f3b49084e79
Oz <frost57@gmail.com>
2020-05-24 21:01:19 -0400

    Added in extra functions to implement all declarations

--- MenuDriver.cpp
+++ MenuDriver.cpp
@@ -13,11 +13,16 @@ using namespace std;
 
 void demoText();
 void demoMenu();
+void extraDemoText(); // implements unimplemented functions of Text.h
+void extraDemoMenu(); // implements unimplemented functions Menu.h
 
 int main() {
 
     //demoText();
     //demoMenu();
+    //extraDemoText(); // implements unimplemented functions of Text.h
+    //extraDemoMenu(); // implements unimplemented functions Menu.h
+
     return 0;
 }
 
@@ -86,4 +91,44 @@ void demoMenu() {
     menu.push_back("Vanilla ice cream");
     menu.push_back("Chocolate chip cookie dough ice cream");
     menu.read_option_number();
+}
+
+void extraDemoText() {
+
+    Text t1 = ("This is t1");
+    Text t2 = ("This is t2");
+
+    cout << "Value of Text t1 before assign: " << t1 << endl;
+
+    t1.assign(t2); // implementation of void assign(const Text&)
+    cout << "Value of Text t1 after assign: " << t1 << endl;
+    cout << "Is t1 empty? " << t1.isEmpty() << endl; // implementation of bool isEmpty() const
+
+    cout << "Length of t1: " << t1.length() << endl; // implementation of int length() const
+
+    t1.clear(); // implementation of void clear()
+    cout << "Value of Text t1 after clear: " << t1 << endl;
+    cout << "Is t1 empty? " << t1.isEmpty() << endl;
+}
+
+void extraDemoMenu() {
+
+    Menu m1;
+    cout << "\nm1 capacity: " << m1.getCapacity() << endl; // implementation of int getCapacity() const
+    cout << "m1 count: " << m1.getCount() << endl; // implementation of int getCount() const
+    cout << "Is m1 empty? " << m1.isEmpty() << endl; // implementation of bool isEmpty() const;
+
+    m1.push_back(Text("first")); // implementation of void push_back(const Text&)
+    m1.push_back(Text("second"));
+    cout << "\nm1 after push_back: " << m1 << endl;
+
+    cout << "\nm1 capacity: " << m1.getCapacity() << endl;
+    cout << "m1 count: " << m1.getCount() << endl;
+    cout << "Is m1 empty? " << m1.isEmpty() << endl; // implementation of bool isEmpty() const;
+    cout << "\n1. value: " << m1.getText(1) << endl; // implementation of const Text getText(int) const
+
+    m1.set_top_message("This is the top message");
+    cout << "\nm1 with top message\n" << m1 << endl;
+    m1.clear_top_message(); //implements void clear_top_message()
+    cout << "\nm1 without top message\n" << m1 << endl;
 }
